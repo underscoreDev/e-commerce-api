8d095dcef57812a8128c7a505f301e691e655888
underscoreDev <godswill@zimmerman.team>
2022-07-10 23:54:29 +0100

    feat: wrote tests

--- src/tests/models/user.test.ts
+++ src/tests/models/user.test.ts
@@ -2,16 +2,23 @@ import { UserModel } from "../../models/user.model";
 const User = new UserModel();
 
 describe("User Model test", () => {
-  it("should have a get All Users method", async () => {
+  const user = {
+    firstname: "ekopimo",
+    lastname: "affia",
+    email: "affia@gmail.com",
+    password: "affia123",
+  };
+
+  it("should have a get All Users method", () => {
     expect(User.getAllUsers).toBeDefined();
   });
-  it("should have a get One User method", async () => {
+  it("should have a get One User method", () => {
     expect(User.getOneUser).toBeDefined();
   });
-  it("should have a update Users method", async () => {
+  it("should have a update Users method", () => {
     expect(User.updateUserInfo).toBeDefined();
   });
-  it("should have a delete Users method", async () => {
+  it("should have a delete Users method", () => {
     expect(User.deleteUser).toBeDefined();
   });
 });
